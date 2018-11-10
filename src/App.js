9156f02f119f926bfea7135d6b316b1ad38e0fb2
maxpaz <matheusf06@gmail.com>
2018-11-10 19:32:14 -0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Api from './components/Api';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>   
+        <Api></Api>
+      </div>
+    );
+  }
+}
+
+export default App;
